bf7ba7d862f09c855fe654446745769f1637bbfd
radiofriendly2 <102418497+radiofriendly2@users.noreply.github.com>
2022-05-27 17:26:43 -0400

    Update thread.c

--- thread.c
+++ thread.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/time.h>
-#include <time.h>
 
-char *time_stamp(){
-
-	char *timestamp = (char *) malloc(sizeof(char) * 16);
-	time_t ltime;
-	ltime=time(NULL);
-	struct tm *tm;
-	tm=localtime(&ltime);
-	
-	sprintf(timestamp,"%04d %02d %02d %02d %02d %02d", tm->tm_year+1900, tm->tm_mon, 
-	tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
-	return timestamp;
-	}
 
 int main()
 {
